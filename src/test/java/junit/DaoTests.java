13977e6825f4ef043523ac7e97598f5478a597c4
rebeccaAtRevature <98355913+rebeccaAtRevature@users.noreply.github.com>
2022-03-02 18:31:32 -0500

    fixed service test

--- src/test/java/junit/DaoTests.java
+++ src/test/java/junit/DaoTests.java
@@ -1,17 +1,23 @@
 package junit;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.Statement;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import dao.EmployeeDao;
+import dao.EmployeeJdbcDaoImpl;
 import exceptions.SystemException;
+import pojo.EmployeePojo;
 import pojo.ReimbursementPojo;
 
 public class DaoTests {
 	
 	// Create a mock of the Statement class to avoid accessing the database
-	EmployeeDao employeeDao = mock(EmployeeJdbcDaoImpl.class);
+	Class<Statement> stmt = (Statement.class);
 	
 	
 	static EmployeeDao employeeDao;
@@ -24,11 +30,10 @@ public class DaoTests {
 	
 	@Test
 	public void testSubmitRequest() {
-		ReimbursementPojo argument = new ReimbursementPojo(100,112,65);
 		try {
-			ReimbursementPojo actualResult = employeeDao.submitRequest(argument);
+			ReimbursementPojo actualResult = employeeDao.submitRequest(new ReimbursementPojo(100,112,65));
 			ReimbursementPojo expectedResult = new ReimbursementPojo(100,112,65);
-			
+			when(stmt.executeQuery(String)).thenReturn(new ReimbursementPojo(100,112,65));
 		} catch (SystemException e) {
 			
 			e.printStackTrace();
