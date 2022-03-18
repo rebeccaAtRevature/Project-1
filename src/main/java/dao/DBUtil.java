448836828c24177c073206aead99af92243bd292
rebeccaAtRevature <98355913+rebeccaAtRevature@users.noreply.github.com>
2022-03-18 11:48:46 -0400

    changed properties file path

--- src/main/java/dao/DBUtil.java
+++ src/main/java/dao/DBUtil.java
@@ -36,7 +36,7 @@ public class DBUtil {
 		
 		FileInputStream fileStream;
 		try {
-			fileStream = new FileInputStream("C:/Users/rebel/eclipse-workspace/ExpenseReimbursementSystem/src/main/resources/JDBCPropertiesFile.properties");
+			fileStream = new FileInputStream("/var/lib/jenkins/workspace/backend-pipeline/src/main/resources/JDBCPropertiesFile.properties");
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 			throw new SystemException();
